890ddd66fdd2e01967d3e7dc1420f349ce377634
Javohir427 <ortiqxojayevjavohir0511@gmail.com>
2022-10-31 20:40:34 +0500

    8

--- Index08.py
+++ Index08.py
@@ -1,3 +1,6 @@
+import re
+
+
 def main(s):
     """
     A string of length five is given. Return the index of the "*" character, return False if not present.
@@ -10,20 +13,29 @@ def main(s):
     if s[0]=='*':
         k=0
         return k
+    else:
+        return "False"
     if s[1]=='*':
         k=1
         return k
+    else:
+        return "False"
     if s[2]=='*':
         k=2
         return k
+    else:
+        return "False"
     if s[3]=='*':
         k=3
         return k
+    else:
+        return "False"
     if s[4]=='*':
         k=4
         return k
     else:
-        k='False'
+        return "False"
+    
         return k
     
 
